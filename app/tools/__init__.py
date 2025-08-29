ad6952d3b9cd7373007d2eef8bf31630c03321e1
enlavan <enlavanov@protonmail.com>
2025-08-29 01:33:23 -0300

    version 2.0.3 Add User API connector

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -8,15 +8,12 @@ from .get_exchanges_list import register as register_exchanges_list
 from .get_exchange_tickers import register as register_exchange_tickers
 from .get_macro_indicator import register as register_macro_indicator
 from .get_stocks_from_search import register as register_stocks_from_search
-
 from .get_user_details import register as register_user_details
+
 from .get_income_statements import register as register_income_statements
 from .get_balance_sheets import register as register_balance_sheets
 from .get_cash_flow_statements import register as register_cash_flow_statements
 from .get_current_stock_price import register as register_current_stock_price
-
-
-
 
 
 def register_all(mcp):
@@ -30,10 +27,11 @@ def register_all(mcp):
     register_exchange_tickers(mcp)
     register_macro_indicator(mcp)
     register_user_details(mcp)
+    register_stocks_from_search(mcp)
 
     register_income_statements(mcp)
     register_balance_sheets(mcp)
     register_cash_flow_statements(mcp)
     register_current_stock_price(mcp)
 
-    register_stocks_from_search(mcp)
+
